48e8f46e4720e81e30ac75dca8037e305989ad70
liupenglily@163.com <liupenglily@163.com>
2024-12-24 16:01:23 +0800

    add elastic

---
+++ elastic/search_base/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/elastic/go-elasticsearch/v7/esapi"
+	"github.com/elastic/go-elasticsearch/v8"
+	jsoniter "github.com/json-iterator/go"
+	"github.com/liupengh3c/esbuilder"
+)
+
+// 最外层数据结构
+type Documents struct {
+	Shards   Shards      `json:"_shards"`
+	Hits     HitOutLayer `json:"hits"`
+	TimedOut bool        `json:"timed_out"`
+	Took     int         `json:"took"`
+}
+type Shards struct {
+	Failed     int `json:"failed"`
+	Skipped    int `json:"skipped"`
+	Successful int `json:"successful"`
+	Total      int `json:"total"`
+}
+type HitOutLayer struct {
+	Hits     []Hits  `json:"hits"`
+	MaxScore float64 `json:"max_score"`
+	Total    Total   `json:"total"`
+}
+type Hits struct {
+	ID     string         `json:"_id"`
+	Index  string         `json:"_index"`
+	Score  float64        `json:"_score"`
+	Source map[string]any `json:"_source"`
+	Type   string         `json:"_type"`
+}
+type Total struct {
+	Relation string `json:"relation"`
+	Value    int    `json:"value"`
+}
+
+func main() {
+	SearchFromSize()
+}
+
+func SearchFromSize() {
+	st := time.Now()
+	defer func() {
+		fmt.Println("cost:", time.Since(st).Milliseconds(), "ms")
+	}()
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	docs := Documents{}
+	cert, _ := os.ReadFile("/Users/liupeng/Documents/study/elasticsearch-8.17.0/config/certs/http_ca.crt")
+	client, err := elasticsearch.NewClient(elasticsearch.Config{
+		Username:  "elastic",
+		Password:  "XBS=adqa799j_Aoz=A+h",
+		Addresses: []string{"https://127.0.0.1:9200"},
+		CACert:    cert,
+	})
+
+	if err != nil {
+		fmt.Println("create client err:", err.Error())
+		return
+	}
+
+	dslQuery := esbuilder.NewDsl()
+	boolQuery := esbuilder.NewBoolQuery()
+	boolQuery.Filter(esbuilder.NewRangeQuery("doc_id").Gte(1))
+	dslQuery.SetQuery(boolQuery)
+	dslQuery.SetFrom(9991)
+	dslQuery.SetSize(10)
+	dslQuery.SetOrder(esbuilder.NewSortQuery("doc_id", "asc"))
+	dsl := dslQuery.BuildJson()
+	search := esapi.SearchRequest{
+		Index: []string{"new_tag_202411"},
+		Body:  strings.NewReader(dsl),
+	}
+	resp, err := search.Do(context.Background(), client)
+	if err != nil {
+		fmt.Println("search err:", err.Error())
+		return
+	}
+	json.NewDecoder(resp.Body).Decode(&docs)
+	fmt.Println(docs)
+}
--- go.mod
+++ go.mod
@@ -1,9 +1,12 @@
 module test
 
-go 1.19
+go 1.23.2
 
 require (
+	github.com/elastic/go-elasticsearch/v7 v7.17.10
+	github.com/elastic/go-elasticsearch/v8 v8.17.0
 	github.com/json-iterator/go v1.1.12
+	github.com/liupengh3c/esbuilder v0.0.0-20241127083003-d29b99ee8e8d
 	github.com/olivere/elastic/v7 v7.0.32
 	github.com/panjf2000/ants/v2 v2.9.1
 	golang.org/x/net v0.24.0
@@ -12,11 +15,17 @@ require (
 )
 
 require (
+	github.com/elastic/elastic-transport-go/v8 v8.6.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	go.opentelemetry.io/otel v1.28.0 // indirect
+	go.opentelemetry.io/otel/metric v1.28.0 // indirect
+	go.opentelemetry.io/otel/trace v1.28.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
 
