12c448bd9272768ebec64a49c46069b71d63d95a
liupenglily@163.com <liupenglily@163.com>
2025-04-12 18:31:52 +0800

    add rabbitmq

--- rabbitmq/async/consumer.py
+++ rabbitmq/async/consumer.py
@@ -1,90 +1,38 @@
-#!/usr/bin/env python
-import asyncio
-import aio_pika
+from datetime import datetime
 import time
+import asyncio
+from rabbitmq_utils import DwRabbitMQ
 
-class DwRabbitMQ:
-    def __init__(self, url, exchange, queue_name):
-         self.async_queue = asyncio.Queue()
-         self.url = url
-         self.exchange = exchange
-         self.queue_name = queue_name
-         self.connection = None
-         self.channel = None
-         self.queue = None
-    async def connect(self):
-        try:
-            self.connection = await aio_pika.connect_robust(
-                self.url
-            )
-            self.channel = await self.connection.channel()
-            self.exchange = await self.channel.declare_exchange(name=self.exchange, type='fanout')
-            self.queue = await self.channel.declare_queue(name=self.queue_name, durable=True)
-        except Exception as e:
-            print(e)
-    async def queue_bind_exchange(self):
-        try:
-            await self.queue.bind(exchange=self.exchange)
-        except Exception as e:
-            print(e)
-    async def set_callback(self, callback):
-        try:
-            await self.queue.consume(callback)
-        except Exception as e:
-            print(e)
-    async def callback(self, message: aio_pika.IncomingMessage):
-        try:
-            # print(f"Consumer 1 received: {message.body.decode()}")
-            self.async_queue.put_nowait((message,message.body.decode()))
-        except Exception:
-            print(f" [!] Rejected: {message.body.decode()}")
-
-    async def is_connected(self):
-        return self.connection.is_closed
-    def get_msg(self):
-        return self.async_queue.get_nowait()
 
 async def main():
-    buffer = []
-    batch_size = 10
+    batch_size = 60
+    max_delay = 60
+    lists = []
+    dw_rabbitmq = DwRabbitMQ("amqp://guest:guest@192.168.3.15:5672/addw", 'dw_jiesuan_queue', 'dw_jiesuan_exchange')
+    await dw_rabbitmq.connect()
+    await dw_rabbitmq.queue_bind_exchange()
+    await dw_rabbitmq.set_callback(dw_rabbitmq.callback)
     last_time = time.time()
-    url = "amqp://guest:guest@192.168.3.15:5672/"
-    dwmq = DwRabbitMQ(url, "exchange_fan", "dw_clip_insert")
-    await dwmq.connect()
-    await dwmq.queue_bind_exchange()
-    await dwmq.set_callback(dwmq.callback)
-    print(" [*] Waiting for messages. To exit press CTRL+C")
     while True:
-        if await dwmq.is_connected():
-            try:
-                print("reconnecting...")
-                dwmq = DwRabbitMQ(url, "exchange_fan", "dw_clip_insert")
-                await dwmq.connect()
-                await dwmq.set_callback(dwmq.callback)
-            except Exception as e:
-                print(e,",retry to connect")
-                await asyncio.sleep(1)
-                continue
         try:
-            msg = dwmq.get_msg()
+            msg = dw_rabbitmq.get_msg()
         except asyncio.QueueEmpty:
             await asyncio.sleep(1)
             msg = None
-        if msg is not None:
-            buffer.append(msg)
-        if len(buffer) < batch_size and time.time() - last_time < 60:
+        if msg:
+            lists.append(msg)
+        if len(lists) < batch_size and time.time() - last_time < max_delay:
             continue
-        last_time = time.time()
-        print("batch size : {},real size : {}, begin to process".format(batch_size,len(buffer)))
-        # 根据实际需求，批量
-        for msg in buffer:
+        # 批量处理消息
+        for i in range(len(lists)):
+            print("[x] Received %r" % lists[i][1])
             try:
-                await msg[0].ack()
+                await lists[i][0].ack()
             except Exception as e:
                 print(e)
-            print(msg[1])
-        buffer = []
-
+        last_time = time.time()
+        lists = []
+            
 if __name__ == '__main__':
     try:
         asyncio.run(main())
---
+++ rabbitmq/async/rabbitmq_utils.py
@@ -0,0 +1,85 @@
+import asyncio
+import aio_pika
+
+class DwRabbitMQ:
+    """DwRabbitMQ."""
+    def __init__(self, url, queue_name, exchange_name, exchange_type='direct'):
+         self.async_queue = asyncio.Queue()
+         self.url = url
+         self.exchange_name = exchange_name
+         self.exchange_type = exchange_type
+         self.queue_name = queue_name
+         self.connection = None
+         self.channel = None
+         self.queue = None
+         self.exchange = None
+
+    async def connect(self):
+        """connect、declare exchange and declare queue"""
+        try:
+            self.connection = await aio_pika.connect_robust(
+                url=self.url
+            )
+            self.channel = await self.connection.channel()
+
+            self.exchange = await self.channel.declare_exchange(name=self.exchange_name, type=self.exchange_type)
+            arguments = {
+                "x-dead-letter-exchange": 'dlk:' + self.exchange_name,
+                "x-dead-letter-routing-key": 'dlk:' + self.queue_name,
+            }
+            # 队列声明为永久队列，持久化
+            self.queue = await self.channel.declare_queue(name=self.queue_name, arguments=arguments, durable=True)
+        except Exception as e:
+            print("occur exception:",e)
+
+    async def queue_bind_exchange(self):
+        """queue_bind_exchange."""
+        try:
+            await self.queue.bind(exchange=self.exchange)
+        except Exception as e:
+            print("queue_bind_exchange error!")
+
+    async def reconnect_and_bind(self):
+        """reconnect and bind."""
+        if self.connection.is_closed:
+            await self.connect()
+            await self.queue_bind_exchange()
+            print('reconnect success')
+
+    async def set_callback(self, callback):
+        """set_callback."""
+        try:
+            await self.queue.consume(callback)
+        except Exception as e:
+            print("set_callback error!")
+
+    async def publish_msg(self, content):
+        """publish_msg."""
+        message = aio_pika.Message(content.encode(),delivery_mode=aio_pika.DeliveryMode.PERSISTENT)
+        try:
+            # 设置为持久化消息
+            await self.exchange.publish(message, routing_key=self.queue_name)
+            print(" [x] Sent %r" % content)
+        except Exception as e:
+            self.reconnect_and_bind()
+            await self.exchange.publish(message, routing_key=self.queue_name)
+            print("reconnect......." ,e)
+
+    async def callback(self, message: aio_pika.IncomingMessage):
+        """callback."""
+        try:
+            # print(f"Consumer 1 received: {message.body.decode()}")
+            self.async_queue.put_nowait((message, message.body.decode()))
+        except Exception:
+            print("callback error!")
+
+
+    def get_msg(self):
+        """get_msg."""
+        return self.async_queue.get_nowait()
+
+    async def close(self):
+        """close."""
+        if not self.connection.is_closed:
+            self.connection.close()
+
