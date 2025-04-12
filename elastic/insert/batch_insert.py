9b748a0001d425f7883b6bd4e99f93f4acd2d431
liupenglily@163.com <liupenglily@163.com>
2025-04-12 18:35:28 +0800

    add es bulk insert

--- elastic/insert/batch_insert.py
+++ elastic/insert/batch_insert.py
@@ -2,6 +2,57 @@ from elasticsearch import AsyncElasticsearch
 from elasticsearch.helpers import async_bulk
 import asyncio
 
+class DwElasticSearch:
+    """DwElasticSearch"""
+    def __init__(self, host, username, password, index_prefix) -> None:
+        self.index_prefix = index_prefix
+        self.host = host
+        self.user_name = username
+        self.password = password
+        self.client = AsyncElasticsearch(
+            hosts=[self.host],  # 集群地址
+            # 可选参数示例:
+            http_auth=(self.user_name, self.password),  # 认证信息
+            # verify_certs=False,  # 关闭SSL验证
+            # ca_certs = '/Users/liupeng/Documents/elastic/elasticsearch-8.17.0/config/certs/http_ca.crt'
+            timeout=30  # 请求超时时间
+        )
+
+    async def is_connecting(self) -> bool:
+        """ is_connecting """
+        if await self.client.ping():
+            return True
+        else:
+            return False
+
+    async def check_connect(self):
+        """ check_connect """
+        if not await self.is_connecting():
+            self.client = AsyncElasticsearch(
+            hosts=[self.host],  # 集群地址
+            # 可选参数示例:
+            http_auth=(self.username, self.password),  # 认证信息
+            # verify_certs=False,  # 关闭SSL验证
+            # ca_certs = '/Users/liupeng/Documents/elastic/elasticsearch-8.17.0/config/certs/http_ca.crt'
+            timeout=30  # 请求超时时间
+        )
+    
+    async def es_bulk_insert(self, docs):
+        """ es_bulk_insert """
+        try:
+            # 检测 es 链接是否正常
+            await self.check_connect()
+            # 执行批量写入
+            success_count, errors = await async_bulk(
+                self.client,
+                docs,
+                chunk_size=500,
+                max_retries=3
+            )
+            print(f"成功写入 {success_count} 条文档")
+            return success_count, errors
+        except Exception as e:
+            print(f"批量写入失败: {str(e)}")
 async def main():
     # 创建异步 Elasticsearch 客户端
     es = AsyncElasticsearch(
