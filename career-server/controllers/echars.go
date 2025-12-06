a8f558f3012e866e21c4c4e8164ec62e6f466232
棉花糖 <liupenglily@163.com>
2025-12-06 12:53:52 +0800

    gin+go-echarts

--- career-server/controllers/echars.go
+++ career-server/controllers/echars.go
@@ -2,26 +2,15 @@ package controllers
 
 import (
 	"career-server/models/page"
-	"io"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// 渲染图表到HTML
-func renderChart(c *gin.Context, chart interface{}) {
-	err := chart.(interface {
-		Render(w io.Writer) error
-	}).Render(c.Writer)
-
-	if err != nil {
-		c.String(http.StatusInternalServerError, "图表渲染失败: "+err.Error())
-		return
-	}
-}
-
-func CreateEcharts(ctx *gin.Context) {
-	bar := page.CreateLines(ctx)
-	ctx.Header("Content-Type", "text/html; charset=utf-8")
-	bar.Render(ctx.Writer)
+func CreateEchartsLines(ctx *gin.Context) {
+	// f, _ := os.Create("lines.html")
+	// defer f.Close()
+	line := page.CreateLineChart(ctx)
+	// line.Render(f)
+	// ctx.File(f.Name())
+	line.Render(ctx.Writer)
 }
--- career-server/models/page/echarts.go
+++ career-server/models/page/echarts.go
@@ -2,8 +2,10 @@ package page
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-echarts/go-echarts/v2/charts"
@@ -99,4 +101,33 @@ func CreateLines(ctx *gin.Context) *charts.Line {
 			Smooth: opts.Bool(true),
 		}))
 	return line
+}
+
+func CreateLineChart(ctx *gin.Context) *charts.Line {
+	line := charts.NewLine()
+
+	line.SetGlobalOptions(
+		charts.WithTitleOpts(opts.Title{
+			Title: "小度眼镜 vs 小度智能音箱",
+		}),
+		charts.WithTooltipOpts(opts.Tooltip{Show: opts.Bool(true)}),
+		charts.WithInitializationOpts(opts.Initialization{
+			Theme: "dark", // 使用暗黑主题
+		}),
+	)
+
+	line.SetXAxis([]string{"1月", "2月", "3月", "4月", "5月", "6月"}).
+		AddSeries("小度眼镜", generateLineData()).
+		AddSeries("小度智能音箱", generateLineData()).
+		SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: opts.Bool(true)}))
+
+	return line
+}
+func generateLineData() []opts.LineData {
+	items := make([]opts.LineData, 0)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < 6; i++ {
+		items = append(items, opts.LineData{Value: r.Intn(10000)})
+	}
+	return items
 }
